73336cef2b92b58214eefbbec11fbcb73e491bcb
Ramazan Zholdas <ghostcomrad@gmail.com>
2022-09-18 04:28:19 +0600

    stop script fixed

--- stopMysqlContainer.sh
+++ stopMysqlContainer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 name=$(tail -1 temp1.txt)
-if [[ "$name" == "" ]]; then
+check=$(docker ps | grep mysql_db)
+if [[ "$check" == "" ]]; then
 	echo "you didn't started you'r container"
+else
+	docker stop $name
 fi
-docker stop $name
+
